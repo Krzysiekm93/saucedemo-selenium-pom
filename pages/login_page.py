8ddf948b223feaffac4a07edbff48d4c15a86f94
Krzysiekm93 <metrakkrzysztof@gmail.com>
2026-04-14 17:52:47 +0000

    Added enter_password and click_login methods into login_page object

--- pages/login_page.py
+++ pages/login_page.py
@@ -6,12 +6,30 @@ class Locators:
     """
     Locators for elements on Login Page
     """
+    USERNAME = (By.ID, 'user-name')
+    PASSWORD = (By.ID, 'password')
     LOGIN_LINK = (By.ID, 'login-button')
 
 class LoginPage(BasePage):
     """
     Login Page Object
     """
+    def enter_username(self, username):
+        """
+        Enter username to login
+        :param username:
+        :return:
+        """
+        self.driver.find_element(*Locators.USERNAME).send_keys(username)
+
+    def enter_password(self, password):
+        """
+        Enter password to login
+        :param password:
+        :return:
+        """
+        self.driver.find_element(*Locators.PASSWORD).send_keys(password)
+
     def click_login(self):
         """
         Click `login` button and goes to `inventory` page
