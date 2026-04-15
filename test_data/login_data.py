7513a3fbf3bb2f6add4678ebb12e29b433d4fde7
Krzysiekm93 <metrakkrzysztof@gmail.com>
2026-04-15 18:56:54 +0000

    Added function to read data from CSV file

---
+++ test_data/login_data.py
@@ -0,0 +1,11 @@
+import csv
+
+
+def get_csv_data(filename):
+    rows = []
+    with open(filename) as csvfile:
+        reader = csv.reader(csvfile)
+        next(reader, None)
+        for row in reader:
+            rows.append(row)
+        return rows
