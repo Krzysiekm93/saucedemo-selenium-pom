c9bdf236049f4e9f0751f94882fd80cf01658318
Krzysiekm93 <metrakkrzysztof@gmail.com>
2026-04-22 20:34:30 +0000

    Added click_checkout method in cart page object

--- pages/cart_page.py
+++ pages/cart_page.py
@@ -7,8 +7,12 @@ class Locators:
     CART_REMOVE_BUTTONS = (By.CSS_SELECTOR, ".cart_list > .cart_item button")
     CHECKOUT_BUTTON = (By.ID, "checkout")
 
+
 class CartPage(BasePage):
 
     def remove_last_item(self):
         remove_buttons = self.driver.find_elements(*Locators.CART_REMOVE_BUTTONS)
         remove_buttons[-1].click()
+
+    def click_checkout(self):
+        self.driver.find_element(*Locators.CHECKOUT_BUTTON).click()
